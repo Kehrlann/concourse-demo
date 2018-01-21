09b6431a815e4bd85b2aab3327330f92671ab276
Daniel Garnier-Moiroux <git@garnier.wf>
2018-01-21 17:35:11 +0100

    Expose IDs for each todo in the backend

--- demo/backend/src/main/kotlin/wf/garnier/todobackend/TodoBackendApplication.kt
+++ demo/backend/src/main/kotlin/wf/garnier/todobackend/TodoBackendApplication.kt
@@ -2,9 +2,17 @@ package wf.garnier.todobackend
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+
 
 @SpringBootApplication
-class TodoBackendApplication
+class TodoBackendApplication: RepositoryRestConfigurerAdapter() {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
+        config.setBasePath("api")
+        config.exposeIdsFor(TodoItem::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(TodoBackendApplication::class.java, *args)
