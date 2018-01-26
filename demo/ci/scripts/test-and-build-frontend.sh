9d8e2e0ca694a43a0cbf86ace2ee1c5cb0181818
Daniel Garnier-Moiroux <git@garnier.wf>
2018-01-26 11:57:26 +0100

    Package the front-end and send it to minio

--- demo/ci/scripts/test-and-build-frontend.sh
+++ demo/ci/scripts/test-and-build-frontend.sh
@@ -3,9 +3,26 @@
 set -eu
 
 OUTPUT_DIR=$PWD/build
+GIT_REPO="$PWD/repo/.git"
+
+# This allows running tasks with `fly execute`
+# Because fly execute does not upload the git repo
+if [ -f "$GIT_REPO/ref" ]; then
+  COMMIT_REF=$(cat $GIT_REPO/ref)
+else
+  COMMIT_REF="local"
+fi
 
 cd repo/demo/frontend
 mv /tmp/node_modules .
 CI=true yarn test
 yarn build
-cp -R build/* $OUTPUT_DIR
+
+# Copy the files for deploying to CF
+mkdir -p $OUTPUT_DIR/dist
+cp -R build/* $OUTPUT_DIR/dist
+
+# Package the artifact to upload to minio
+TAR_FILE="frontend-$COMMIT_REF.tgz"
+tar -czf $TAR_FILE -C build .
+mv $TAR_FILE $OUTPUT_DIR
