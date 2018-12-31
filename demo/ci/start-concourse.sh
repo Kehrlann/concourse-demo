d36bb8f3d7e28933fdf30eac90a99648b4eaead1
Daniel Garnier-Moiroux <git@garnier.wf>
2018-12-31 17:09:26 +0100

    Cleanup docker-compose, make it work with latest concourse version
    - Remove unused named volumes
    - Bump the env vars to match newest concourse format
    - Create a start script that starts everything

--- demo/ci/start-concourse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CONCOURSE_EXTERNAL_URL="http://172.19.0.3:8080" docker-compose up -d
---
+++ demo/ci/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo 'Starting PCF dev ...'
+CF_STATUS=$(cf dev status)
+if [[ "$CF_STATUS" == *"Running"* ]]; then
+  echo "Already running, skipping..."
+elif [[ "$CF_STATUS" == "Suspended" ]]; then
+  cf dev resume
+  echo '  OK'
+elif [[ "$CF_STATUS" == *"Stopped"* ]]; then
+  cf dev start
+  echo '  OK'
+else
+  echo 'Unknown state, aborting ...'
+  echo "$CF_STATUS"
+  exit 1337
+fi
+
+echo
+echo 'Starting ssh server (needs sudo) ...'
+sudo service ssh start
+echo '  OK'
+echo
+
+echo 'Starting concourse ...'
+docker-compose up -d -V
+echo '  OK'
