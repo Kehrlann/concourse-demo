45b4f211ddde54fc6f9536a2b219a480d08b4dd9
Daniel Garnier-Moiroux <git@garnier.wf>
2019-01-03 16:26:32 +0100

    Log in to PCF, clean and setup CF+minio in start script

--- demo/ci/start.sh
+++ demo/ci/start.sh
@@ -24,6 +24,45 @@ else
 fi
 
 echo
+echo 'Logging in to PCF ...'
+cf login -a api.local.pcfdev.io --skip-ssl-validation -u admin -p admin -o pcfdev-org -s pcfdev-space
+echo '  OK'
+
+echo
 echo 'Starting concourse ...'
 docker-compose up -d -V
 echo '  OK'
+
+echo
+echo 'Waiting for containers to become healthy ...'
+echo -n '.'
+while [[ -z $(docker-compose ps minio | grep healthy) ]]; do
+  sleep 5
+  echo -n '.'
+done
+echo '  OK'
+
+echo
+echo 'Cleaning up CF'
+./clean-cf.sh | true
+echo '  OK'
+
+echo
+echo 'Cleaning up minio'
+./clean-minio.sh | true
+echo '  OK'
+
+echo
+echo 'Setting up CF'
+./setup-cf.sh | true
+echo '  OK'
+
+echo
+echo 'Setting up minio'
+./setup-minio.sh | true
+echo '  OK'
+
+echo
+echo
+
+echo 'All good now, ready to fly !'
