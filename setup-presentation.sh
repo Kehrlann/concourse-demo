a959f51440164fa1cbf934446f16d2b4a81349a2
Daniel Garnier-Moiroux <git@garnier.wf>
2019-01-10 15:23:39 +0100

    Add setup script to get reveal.js

---
+++ setup-presentation.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/hakimel/reveal.js.git presentation
+rm presentation/index.html
+ln -s $PWD/presentation.html presentation/index.html
+ln -s $PWD/images presentation/img
+cd presentation
+npm install
