8ce6ecd90fbc49dc99224d8702c802f03b87dd3b
Daniel Garnier-Moiroux <git@garnier.wf>
2019-01-11 17:23:46 +0100

    WiP kubernetes, with ingress

---
+++ demo/infrastructure/test-ingress.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080
+kubectl run echoserver --image=gcr.io/google_containers/echoserver:1.4 --port=8080
+kubectl expose kubernetes-bootcamp --type=NodePort
+kubectl expose echoserver --type=NodePort
