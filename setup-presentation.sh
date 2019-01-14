b0e2ad7659eea8c918da57fc82e32eae5e299c9c
Daniel Garnier-Moiroux <git@garnier.wf>
2019-01-14 12:08:00 +0100

    Fix setup presentation script in case images are already linked

--- setup-presentation.sh
+++ setup-presentation.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/hakimel/reveal.js.git presentation
 rm presentation/index.html
 ln -s $PWD/presentation.html presentation/index.html
+rm presentation/img
 ln -s $PWD/images presentation/img
 cd presentation
 npm install
