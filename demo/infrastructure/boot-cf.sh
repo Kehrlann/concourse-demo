81ef798130dc028d73dac0475e22bba65d7ac322
Daniel Garnier-Moiroux <git@garnier.wf>
2019-08-06 10:45:44 +0200

    Update starting / logging in to PCF

--- demo/infrastructure/boot-cf.sh
+++ demo/infrastructure/boot-cf.sh
@@ -2,22 +2,11 @@
 
 set -e
 
-CF_STATUS=$(cf dev status)
-if [[ "$CF_STATUS" == *"Running"* ]]; then
-  echo "Already running, skipping..."
-elif [[ "$CF_STATUS" == "Suspended" ]]; then
-  cf dev resume
-  echo 'Resumed'
-elif [[ "$CF_STATUS" == *"Stopped"* ]] || [[ "$CF_STATUS" == "Not Created" ]]; then
-  cf dev start
-  echo 'Started'
-else
-  echo 'Unknown state, aborting ...'
-  echo "$CF_STATUS"
-  exit 1337
-fi
+echo 'Starting PCF dev'
+cf dev start
+echo '  OK'
 
 echo
 echo 'Logging in to PCF ...'
-cf login -a api.local.pcfdev.io --skip-ssl-validation -u admin -p admin -o pcfdev-org -s pcfdev-space
+cf login -a api.dev.cfdev.sh --skip-ssl-validation -u admin -p admin -o cfdev-org -s cfdev-space
 echo '  OK'
